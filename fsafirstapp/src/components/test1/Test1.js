987575bcce79c6e16d43d41fd94b355a289cc4dd
rajesh <rajesh.t7982@gmail.com>
2023-11-07 08:28:09 +0530

    react-class-5

--- fsafirstapp/src/components/test1/Test1.js
+++ fsafirstapp/src/components/test1/Test1.js
@@ -1,18 +1,64 @@
 import "./Test1.css";
+import { useState } from "react";
 
 //create Test1 component
- function Test1(p) {
-  //{ x : {}}
-  console.log(p);
+function Test1() {
   //state
+  let [counter, setCounter] = useState(0);
+  let [emp, setEmp] = useState({ empid: 100, name: "kiran", age: 21 });
+
+  //change emp
+  function changeEmp() {
+    setEmp({ ...emp, empid: 200, name: "Mahesh" }); //{empid:100,name:'kiran',age:21,empid:200}
+  }
+
+  function addNewProperty() {
+    setEmp({ ...emp, city: "Hyd" });
+  }
+
+  function deleteEmpid(){
+    let copyEmp={...emp}
+    delete copyEmp.empid;
+    setEmp(copyEmp)
+  }
+
+
+  //event handler to change counter
+  function incrementCounter() {
+    //change state
+    setCounter(counter + 1);
+  }
+  function decrementCounter() {
+    //change state
+    setCounter(counter - 1);
+  }
 
   return (
     <div>
-      <h1 className="header-test1">Test1 component</h1>
-      <h2>PID: {p.x.pid}</h2>
-      <h2>PID: {p.x.name}</h2>
-      <h2>PID: {p.x.age}</h2>
-      <img src={p.x.imgUrl} alt="" />
+      <h1 className="display-2 text-info">{counter}</h1>
+      <button className="btn btn-success" onClick={incrementCounter}>
+        Increment Counter
+      </button>
+      <button className="btn btn-danger" onClick={decrementCounter}>
+        Decrement Counter
+      </button>
+
+      <div>
+        <h2>Details of Employee</h2>
+        <p className="lead">{emp.empid}</p>
+        <p className="lead">{emp.name}</p>
+        <p className="lead">{emp.age}</p>
+        <p className="lead">{emp.city}</p>
+        <button className="btn btn-info" onClick={changeEmp}>
+          Chnage Emp details
+        </button>
+        <button className="btn btn-primary" onClick={addNewProperty}>
+          Add new property to Emp
+        </button>
+        <button className="btn btn-danger" onClick={deleteEmpid}>
+          Delete Emp ID
+        </button>
+      </div>
     </div>
   );
 }
--- fsafirstapp/src/components/test2/Test2.js
+++ fsafirstapp/src/components/test2/Test2.js
@@ -1,10 +1,39 @@
 import './Test2.css'
+import {useState} from 'react'
 
 function Test2(){
     //state
+    let [skills,setSkills]=useState(['js','css'])
+
+    //add new skill
+    function addNewSkill(){
+        setSkills(['react',...skills])
+    }
+    //delete existing skill
+    function deleteSkill(){
+        let copySkills=[...skills]
+        copySkills.pop()
+        setSkills(copySkills)
+    }
+    //modify skill
+    function modifySkill(){
+        let copySkills=[...skills]
+        copySkills[1]='bootstrap'
+        setSkills(copySkills)
+    }
 
     return(
-        <h1 className="header-test2">Test2 componnet</h1>
+        <div>
+        <h1 className="display-1 text-primary">Change state of Array</h1>
+        {
+            skills.map(skill=><h1 key={skill}>{skill}</h1>)
+        }
+
+        <button className="btn btn-success" onClick={addNewSkill}>Add new Skill</button>
+        <button className="btn btn-danger" onClick={deleteSkill}>Delete Skill</button>
+        <button className="btn btn-warning" onClick={modifySkill}>Modify Skill</button>
+
+        </div>
     )
 }
 
